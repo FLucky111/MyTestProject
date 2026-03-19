0d416a5708c2fe8afd84c23848b1c6f9d7a25ab9
Виктория <redko3823@gmail.com>
2026-03-19 22:05:42 +0800

    при обновлении страницы пользователю не нужно будет перезаходить в аккаунт

--- frontend/mysite-web/src/App.jsx
+++ frontend/mysite-web/src/App.jsx
@@ -1,10 +1,35 @@
 import Login from "./pages/Login"
 import Profile from "./pages/Profile"
 import Register from "./pages/Register.jsx"
-import { useState } from "react"
+import {useEffect, useState} from "react"
+import api from "./api/api"
 
 function App() {
     const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const token = localStorage.getItem("token")
+        if (token) {
+            api.get("/users/me")
+                .then(res => {
+                    setUser(res.data)
+                })
+                .catch(() => {
+                    localStorage.removeItem("token")
+                    setUser(null)
+                })
+                .finally(() => {
+                    setLoading(false)
+                })
+        }
+        else {
+            setLoading(false)
+        }
+    }, []);
+    
+    if(loading)
+        return <p>Загрузка....</p>
     
     return (
         <div>
